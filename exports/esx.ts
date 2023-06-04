75e579df06596d903d2ef459c7f5c3f89c397c98
undefined <johann@objekt.stream>
2023-06-04 18:20:24 +0200

    ~ extend esx types

--- exports/esx.ts
+++ exports/esx.ts
@@ -1,7 +1,11 @@
 import type { ESXClient } from 'fivem-esx-js/client/esx_client'
 import type { ESXServer } from 'fivem-esx-js/server/esx_server'
 
+interface ESXClientExtended extends ESXClient {
+  Progressbar(message: string, length: number, options: any): unknown
+}
+
 const esx = globalThis.exports.es_extended.getSharedObject()
-export const ESX: ESXClient | undefined = esx
-export const ESX_Client: ESXClient | undefined = esx
+export const ESX: ESXClientExtended | undefined = esx
+export const ESX_Client: ESXClientExtended | undefined = esx
 export const ESX_Server: ESXServer | undefined = esx
