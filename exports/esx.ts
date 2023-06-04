a9052c088e211fc7cc6aeb97d1406ce70f2f3a78
undefined <johann@objekt.stream>
2023-06-04 17:16:42 +0200

    -

--- exports/esx.ts
+++ exports/esx.ts
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import type { ESXClient } from 'fivem-esx-js/client/esx_client'
 import type { ESXServer } from 'fivem-esx-js/server/esx_server'
 
-const esx = globalThis?.exports?.es_extended?.getSharedObject()
+const esx = globalThis.exports.es_extended.getSharedObject()
 export const ESX: ESXClient | undefined = esx
 export const ESX_Client: ESXClient | undefined = esx
 export const ESX_Server: ESXServer | undefined = esx
